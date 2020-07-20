d0e1c7c978ca63cf43c6f2ad7ef18973369b131c
Manuel Eduardo Baez Diaz <baezdiazm@yahoo.com>
2020-07-20 18:15:04 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,26 @@
 class Waiter
+  attr_accessor :name, :exp
+  @@all = []
+  def initialize(name, exp)
+    @name = name
+    @exp = exp
+    @@all << self
+  end
 
+  def self.all
+    @@all
+  end
+
+  def new_meal(customer, total, tip = 0)
+    Meal.new(self, customer, total, tip)
+  end
+
+  def meals
+    meals = Meal.all.select {|meal| meal.waiter == self}
+    meals
+  end
+
+  def best_tipper
+    Meal.all.tip
+  end
 end
